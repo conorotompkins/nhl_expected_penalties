6c550fa5261532480724ab9e31e9ff543c56b27a
Conor Tompkins <conorotompkins@Conors-MBP.localdomain>
2022-11-06 17:02:00 -0500

    commented out hockeyref scraping for now. added penalties taken

--- scripts/eda.R
+++ scripts/eda.R
@@ -5,24 +5,30 @@ library(broom)
 
 pbp <- load_pbp('2018-19')
 
-season_rosters <- get_rosters(season = 2019) |> 
-  select(player, position) |> 
-  mutate(player = str_replace(player, " ", ".")) |> 
-  mutate(position_clean = case_when(position == "G" ~ "G",
-                                    str_detect(position, "D") ~ "D",
-                                    TRUE ~ "F"))
-
-season_rosters |> 
-  count(position_clean, position) |> 
-  arrange(position_clean)
-
-season_rosters <- season_rosters |> 
-  select(-position) |> 
-  rename(position = position_clean)
+# season_rosters <- get_rosters(season = 2019) |> 
+#   select(player, position) |> 
+#   mutate(player = str_replace(player, " ", ".")) |> 
+#   mutate(position_clean = case_when(position == "G" ~ "G",
+#                                     str_detect(position, "D") ~ "D",
+#                                     TRUE ~ "F"))
+# 
+# season_rosters |> 
+#   count(position_clean, position) |> 
+#   arrange(position_clean)
+# 
+# season_rosters <- season_rosters |> 
+#   select(-position) |> 
+#   rename(position = position_clean)
 
 glimpse(pbp)
 
-glimpse(season_rosters)
+#glimpse(season_rosters)
+
+skater_stats <- calculate_individual(pbp)
+
+glimpse(skater_stats)
+
+#skater_toi <- calculate_toi(pbp)
 
 pbp |>
   filter(event_type %in% c("SHOT","MISSED_SHOT","GOAL")) |>
@@ -63,19 +69,30 @@ pbp |>
 penalty_df <- pbp |>
   filter(event_type == "PENALTY",
          strength_state == "5v5") |> 
-  select(event_type, secondary_type, event_player_2_type, event_player_2_name, event_player_2_id, strength_state) |> 
-  drop_na(event_player_2_name) |> 
-  rename(pen_type = secondary_type,
-         player = event_player_2_name,
-         player_id = event_player_2_id)
+  select(strength_state, event_type, secondary_type,
+         event_player_1_type, event_player_1_name, event_player_1_id,
+         event_player_2_type, event_player_2_name, event_player_2_id)
 
 penalty_df
 
 player_pen_draw <- penalty_df |> 
-  count(player, player_id, sort = T, name = "penalties_drawn")
+  drop_na(event_player_2_name) %>% 
+  count(event_player_2_name, event_player_2_id, sort = T, name = "penalties_drawn") %>% 
+  rename(player = event_player_2_name,
+         player_id = event_player_2_id)
 
 player_pen_draw |> 
   ggplot(aes(penalties_drawn)) +
+  geom_density(fill = "grey")
+
+player_pen_taken <- penalty_df |> 
+  drop_na(event_player_1_name) %>% 
+  count(event_player_1_name, event_player_1_id, sort = T, name = "penalties_taken") %>% 
+  rename(player = event_player_1_name,
+         player_id = event_player_1_id)
+
+player_pen_taken |> 
+  ggplot(aes(penalties_taken)) +
   geom_density(fill = "grey")
 
 pbp |>
@@ -108,27 +125,28 @@ player_metrics |>
   facet_wrap(vars(name), scales = "free")
 
 reg_data <- player_pen_draw |> 
-  left_join(player_metrics, by = c("player")) |> 
-  left_join(season_rosters, by = c("player")) |> 
-  mutate(position = coalesce(position, "unknown")) |> 
-  filter(position != "G")
+  left_join(player_pen_taken, by = c("player", "player_id")) |> 
+  left_join(player_metrics, by = c("player", "player_id")) |> 
+  #left_join(season_rosters, by = c("player")) |> 
+  #mutate(position = coalesce(position, "unknown")) |> 
+  filter(shots >= 100)
 
 glimpse(reg_data)
 
 reg_data |> 
   filter(position == "unknown")
 
-season_rosters |> 
-  filter(str_detect(player,  "Gabriel"))
+# season_rosters |> 
+#   filter(str_detect(player,  "Gabriel"))
 
 reg_data |> 
-  pivot_longer(cols = c(shots, goals, xg_median, shot_distance_median)) |> 
+  pivot_longer(cols = c(shots, goals, xg_median, shot_distance_median, penalties_taken)) |> 
   ggplot(aes(penalties_drawn, value, color = name)) +
   geom_jitter(alpha = .3) +
   geom_smooth() +
-  facet_wrap(vars(name, position), scales = "free", ncol = 3)
+  facet_wrap(vars(name), scales = "free", ncol = 3)
 
-lm_model <- lm(penalties_drawn ~ shots + position + goals + xg_median + shot_distance_median, data = reg_data)
+lm_model <- lm(penalties_drawn ~ shots + goals + xg_median + shot_distance_median + penalties_taken, data = reg_data)
 
 tidy(lm_model) |> 
   mutate(term = fct_reorder(term, estimate)) |> 
